34e4ff6e4b2b8f8271de5aa1c588b403901d76be
Artur <artur.h.0427@gmail.com>
2023-03-01 11:06:13 +0100

    Add constructor to `Peers` struct

--- src/peers.rs
+++ src/peers.rs
@@ -2,7 +2,7 @@
 use futures_channel::mpsc::UnboundedSender;
 use tokio_tungstenite::tungstenite::Message;
 use std::{
-    sync::{ Arc, Mutex, LockResult, MutexGuard },
+    sync::{ Arc, Mutex, MutexGuard },
     collections::HashMap,
     net::SocketAddr
 };
@@ -14,15 +14,21 @@ pub type PeerMap = HashMap<SocketAddr, Tx>;
 /// Each connection is called a `peer`. Each peer has 
 /// a `std::net::SocketAddr` tied to it and an `futures_channel::mpsc::UnboundedSender`
 /// which allows for sending messages to that peer.
+#[derive(Clone)]
 pub struct Peers(Arc<Mutex<PeerMap>>);
 
 /* Method impls */
 impl Peers {
+    /// Construct a new peers map
+    pub fn new() -> Self {
+        Self(Arc::new(Mutex::new(HashMap::new())))
+    }
+
     /// Try locking the peer map and getting inner values (mutex)
-    pub fn lock(&self) -> Result<PeerMap, ()> {
+    pub fn lock(&self) -> Result<MutexGuard<PeerMap>, ()> {
         match self.0.lock() {
-            Ok(e) => Ok(*e),
-            Err(_) => Err(())
+            Ok(e) => Ok(e),
+            Err(_) => return Err(())
         }
     }
 }
