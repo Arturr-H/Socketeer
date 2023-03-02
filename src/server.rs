be0b9b8b9afbb32604a6fc2d58ef88c7afd93298
Artur <artur.h.0427@gmail.com>
2023-03-02 16:48:41 +0100

    Add global data handler

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,5 @@
 /* Imports */
-use std::{collections::HashMap, sync::Arc};
+use std::{collections::HashMap, sync::Arc, any::Any};
 use tokio::{ net::TcpListener, sync::Mutex };
 use crate::{
     peers::Peers,
@@ -23,18 +23,28 @@ pub struct Server {
 
     /// All the peers (requests) are stored in here
     peers: Peers,
+
+    /// Global data shared between all endpoints
+    global: Arc<tokio::sync::Mutex<(dyn Any + Send)>>
 }
 
 /* Method impls */
 impl Server {
     /// Construct a new server struct
     pub fn new() -> Self {
-        Self { address: None, port: None, endpoints: HashMap::new(), peers: Peers::new(), data: Arc::new(Mutex::new(0usize)) }
+        Self { address: None, port: None, endpoints: HashMap::new(), peers: Peers::new(), global: Arc::new(Mutex::new(0usize)) }
     }
 
     /// Create a new websocket endpoint
     pub fn endpoint(&mut self, name: &str, func: EndpointFunction) -> &mut Self {
         self.endpoints.insert(name.to_owned(), Box::new(func));
+        self
+    }
+
+    /// Set the global data type which will be shared across all endpoints.
+    /// `data` has to implement `Send`, and can be accessed via req
+    pub fn set_global<Data: Send + 'static>(&mut self, data: Data) -> &mut Self {
+        self.global = Arc::new(Mutex::new(data));
         self
     }
 
@@ -51,7 +61,7 @@ impl Server {
         while let Ok((stream, addr)) = server.accept().await {
             tokio::spawn(
                 handle_connection(
-                    self.data.clone(), 
+                    self.global.clone(), 
                     self.endpoints.clone(), 
                     self.peers.clone(),
                     stream, 
